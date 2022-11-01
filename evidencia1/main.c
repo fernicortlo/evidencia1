42776abbc14f18028bca73dae12356be1f72af21
fernicortlo <a01026613@tec.mx>
2022-11-01 13:44:17 -0600

    Initial Commit

---
+++ evidencia1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  evidencia1
+//
+//  Created by Fernanda Cortes on 01/11/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
